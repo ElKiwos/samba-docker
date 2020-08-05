0b30c50ee0fbff883942ba33905cd7b8c9b01d17
Kiwi <christian.kufner@scandio.de>
2020-08-05 15:09:50 +0200

    build: initial commit

---
+++ README.md
@@ -0,0 +1,108 @@
+# docker-samba-ad-dc
+
+[![Project Status: Active](https://www.repostatus.org/badges/latest/active.svg)](https://www.repostatus.org/#active)
+
+ubuntu based docker container for samba active directory server
+
+This was inspired by other older docker containers that either don't work or use completely outdated versions of samba. To fullfill my own requirements, I wrote this from scratch.
+
+# Limitations
+
+The container *needs* a macvtap based network. You may try other network types, however you have been warned, it will most likely not work without problems.
+
+## Running
+
+Setup your own samba.env file with your choice of options by copying the example.
+
+``cp samba.env.example samba.env``
+
+Then start the container. It will immediatly provision the domain and start samba afterwards.
+
+### Environment Variables explained
+
+The environment variables are passed on to the domain provising of samba-tool.
+For in-depth information see the fine [Samba Wiki](https://wiki.samba.org/index.php/Setting_up_Samba_as_an_Active_Directory_Domain_Controller#Parameter_Explanation) on
+[samba-tool](https://wiki.samba.org/index.php/Setting_up_Samba_as_an_Active_Directory_Domain_Controller#Provisioning_a_Samba_Active_Directory).
+
+parameter | purpose
+--------- | --------
+``` SAMBA_DC_HOSTNAME=dc ``` | Hostname of the containerized domain controller. If you change this, you will need to change the hostname in the dockerfile as well.
+``` SAMBA_DC_HOSTIP= ``` | should be left alone (is a leftover from trying to make it work with host network)
+```SAMBA_DC_ADMIN_PASSWD=replacePassword``` | Yeah for real, please change this to something secure.
+```SAMBA_DC_REALM=my.domain``` | replace with your active directory domain name
+```SAMBA_DNS_BACKEND=SAMBA_INTERNAL``` | should be left alone
+```SAMBA_DNS_FORWARDER=192.168.1.254``` | Sets the dns server that dns queries are forwarded to.
+```SAMBA_NOCOMPLEXPWD=true``` | If true then sets the password complexity to off, expiry and password history is turned off as well, otherwise password complexity is left alone. 
+
+## Volumes
+
+As the docker file defines, and the docker-compose file configures, those are the volumes:
+
+volume      | purpose
+----------- | -------
+/etc/samba | config files
+/var/lib/samba | sysvolume
+/var/log/samba | logfiles
+/srv/shares | shares and their files
+
+You can point those to the paths of your liking, e.g:
+
+```
+      - /srv/docker/samba/etc:/etc/samba:rw
+      - /srv/docker/samba/sys:/var/lib/samba:rw
+      - /srv/docker/samba/log:/var/log/samba:rw
+      - /srv/docker/samba/shares:/srv/shares:rw
+```
+
+## Joining a domain as a dc
+
+Be warned, this are my own notes and may or may not work!
+
+add to .env file:
+
+parameter | purpose
+--------- | --------
+SAMBA_PROVISION_TYPE=JOIN | provisioning type: DC for standalone domain controller
+or JOIN for joining an existing domain
+
+
+###
+* hot backup idmap
+
+
+* rsync sysvol
+[SysVol replication](https://wiki.samba.org/index.php/SysVol_replication_(DFS-R))
+
+### fsmo transfer / seizure
+Ideas shamelessly copied from 
+[Samba documentation - fsmo](https://wiki.samba.org/index.php/Transferring_and_Seizing_FSMO_Roles#Transferring_an_FSMO_Role)
+
+```samba-tool fsmo show```
+
+```samba-tool fsmo transfer --role=...```
+
+### check things 
+
+```samba-tool fsmo show```
+
+### demote old dc
+!!! On the old dc run the following command
+
+```
+samba-tool domain demote -Uadministrator
+```
+Shameless copy from
+[Samba documentation](https://wiki.samba.org/index.php/Demoting_a_Samba_AD_DC)
+
+## Building
+
+You can either use the following command to build the docker image for the samba ad dc:
+
+``build.sh``
+
+or simple use docker-compose
+
+``docker-compose build``
+
+There are other docker-compose files present that will most likely not work as expected.
+
---
+++ smb-ad-dc/docker-entrypoint.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+set -e
+RED='\033[0;31m'
+NC='\033[0m' #no color
+YEL='\033[1;33m'
+GR='\033[1;32m'
+
+ENVVARS="SAMBA_DC_DOMAIN SAMBA_DC_REALM SAMBA_DC_ADMIN_PASSWD"
+
+SAMBA_PROVISION_TYPE=${SAMBA_PROVISION_TYPE:-SERVER}
+SAMBA_PROVISION_TYPE=${SAMBA_PROVISION_TYPE^^}
+REMOTE_DC=${REMOTE_DC:-NONE}
+SAMBA_DC_REALM=${SAMBA_DC_REALM:-XDOM.EXAMPLE.LOCAL}
+SAMBA_DC_REALM=${SAMBA_DC_REALM^^}
+LOWERCASE_DOMAIN=${SAMBA_DC_REALM,,}
+SAMBA_DC_DOMAIN=${LOWERCASE_DOMAIN%%.*}
+SAMBA_DC_ADMIN_PASSWD=${SAMBA_DC_ADMIN_PASSWD:-youshouldchangethis}
+SAMBA_DNS_BACKEND=${SAMBA_DNS_BACKEND:-SAMBA_INTERNAL}
+SAMBA_DNS_FORWARDER=${SAMBA_DNS_FORWARDER:-NONE}
+SAMBA_NOCOMPLEXPWD=${SAMBA_NOCOMPLEXPWD:-false}
+SAMBA_DC_HOSTNAME=${SAMBA_DC_HOSTNAME:-noname}
+SAMBA_DC_HOSTIP=${SAMBA_DC_HOSTIP:-NONE}
+
+HOSTIP_OPTION=""
+if [ "${SAMBA_DC_HOSTIP}" != "NONE" ]; then
+	HOSTIP_OPTION="--host-ip=$SAMBA_DC_HOSTIP"
+fi
+
+perl -E 'say "=" x 100'
+echo -e "${YEL}SAMBA_PROVISION_TYPE:\t\t${NC}${SAMBA_PROVISION_TYPE}"
+echo -e "${YEL}REMOTE_DC:\t\t${NC}${REMOTE_DC}"
+echo -e "${YEL}SAMBA_DC_REALM:\t\t${NC}${SAMBA_DC_REALM}"
+echo -e "${YEL}SAMBA_DC_DOMAIN:\t${NC}${SAMBA_DC_DOMAIN}"
+echo -e "${YEL}SAMBA_DC_ADMIN_PASSWD:\t${NC}${SAMBA_DC_ADMIN_PASSWD}"
+echo -e "${YEL}SAMBA_DNS_BACKEND:\t${NC}${SAMBA_DNS_BACKEND}"
+echo -e "${YEL}SAMBA_DNS_FORWARDER:\t${NC}${SAMBA_DNS_FORWARDER}"
+echo -e "${YEL}SAMBA_NOCOMPLEXPWD:\t${NC}${SAMBA_NOCOMPLEXPWD}"
+echo -e "${YEL}SAMBA_DC_HOSTNAME:\t\t${NC}${SAMBA_DC_HOSTNAME}"
+echo -e "${YEL}SAMBA_DC_HOSTIP:\t\t${NC}${SAMBA_DC_HOSTIP}"
+
+function patch_resolv {
+cat > /etc/resolv.conf <<EOF
+nameserver $1
+search ${SAMBA_DC_REALM,,}
+options ndots:0
+EOF
+}
+
+if [ ! -f /etc/samba/smb.conf ]; then
+    if [ ${SAMBA_PROVISION_TYPE} == "SERVER" ] ; then
+        echo -e "samba-tool domain provision --domain=${SAMBA_DC_DOMAIN} \
+            --adminpass=${SAMBA_DC_ADMIN_PASSWD} \
+            --server-role=dc \
+            --realm=${SAMBA_DC_REALM} \n \
+            --dns-backend=${SAMBA_DNS_BACKEND} \
+            --host-name=${SAMBA_DC_HOSTNAME} \
+            --use-rfc2307 \
+            ${HOSTIP_OPTION}"
+        samba-tool domain provision --domain="${SAMBA_DC_DOMAIN}" \
+            --adminpass="${SAMBA_DC_ADMIN_PASSWD}" \
+            --server-role=dc \
+            --realm="${SAMBA_DC_REALM}" \
+            --dns-backend="${SAMBA_DNS_BACKEND}" \
+            --host-name="${SAMBA_DC_HOSTNAME}" \
+            --use-rfc2307 \
+            ${HOSTIP_OPTION}
+#            --option="allow dns updates = disabled"
+        sleep 5
+        if [ "${SAMBA_NOCOMPLEXPWD,,}" = "true" ]; then
+            echo "samba-tool domain passwordsettings set --complexity=off"
+            samba-tool domain passwordsettings set --complexity=off
+            samba-tool domain passwordsettings set --history-length=0
+            samba-tool domain passwordsettings set --min-pwd-age=0
+            samba-tool domain passwordsettings set --max-pwd-age=0
+        fi
+    else
+	 # time should be checked prior!!!
+         # join preparations
+	 patch_resolv $REMOTE_DC
+	 cat > /etc/krb5.conf <<EOF
+[libdefaults]
+    dns_lookup_realm = false
+    dns_lookup_kdc = true
+    default_realm = ${SAMBA_DC_REALM}
+EOF
+         # test if dns and kerberos connection work
+         #apt-get install expect
+	 #cat > /root/kinit_test.expect <<EOF
+##!/bin/expect
+#
+#set pwd "${SAMBA_DC_ADMIN_PASSWD}"
+#
+#spawn /usr/bin/kinit administrator
+#
+#expect "assword for administrator@${SAMBA_DC_REALM}: "
+#send "\$pwd"
+#EOF
+#	 /usr/bin/expect /root/kinit_test.expect
+	 echo "${SAMBA_DC_ADMIN_PASSWD}" | kinit administrator
+	 klist
+	 RC=$?
+	 # now join the domain
+	 if [ $RC -eq 0 ]; then
+             echo -e "${GR} ************************************"
+             echo -e "${GR} JOINING DOMAIN ${SAMBA_DC_REALM} now" 
+             echo -e "${GR} ************************************"
+	     samba-tool domain join ${SAMBA_DC_REALM} DC -k yes
+         fi
+    fi
+
+    if [ "${SAMBA_DNS_FORWARDER}" != "NONE" ]; then
+        sed -i "/\[global\]/a \
+            \\\tdns forwarder = ${SAMBA_DNSFORWARDER}\
+            " /etc/samba/smb.conf
+    fi
+fi
+
+# link kerberos config (so that it may be modified)
+rm /etc/krb5.conf
+ln -s /var/lib/samba/private/krb5.conf /etc/krb5.conf
+# extend lookup to use winbind
+sed -i -e "s/\(passwd:.*\)/\1 winbind/" \
+    -e "s/\(group:.*\)/\1 winbind/" /etc/nsswitch.conf
+
+
+patch_resolv 127.0.0.1
+
+if [ "$1" = 'samba' ]; then
+    exec samba -i < /dev/null
+fi
+
+exec "$@"
